154b8f108ec4da6b9ae45d29578c5e3791c638f9
Peter Skrivanek <123364568+S00236650@users.noreply.github.com>
2024-01-06 09:54:21 +0000

    Q1B - Q1D

---
+++ JanuaryExam/BudgetItem.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JanuaryExam
+{
+    internal class BudgetItem : IComparable<BudgetItem>
+    {
+        public string Name { get; set; }
+        public decimal Amount { get; set; }
+        public enum ItemType { Income, Expense }
+        public DateTime Date { get; set; }
+        public bool Recurring { get; set; }
+
+        public BudgetItem() { }
+
+        public int CompareTo(BudgetItem other)
+        {
+            
+        }
+    }
+}
