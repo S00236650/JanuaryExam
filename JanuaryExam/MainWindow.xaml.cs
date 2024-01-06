74ca46c6ee029192ef0f44cc270a0054daa754e6
Peter Skrivanek <123364568+S00236650@users.noreply.github.com>
2024-01-06 10:49:42 +0000

    Q1H + Q1J

--- JanuaryExam/MainWindow.xaml.cs
+++ JanuaryExam/MainWindow.xaml.cs
@@ -20,23 +20,92 @@ namespace JanuaryExam
     /// </summary>
     public partial class MainWindow : Window
     {
+        decimal totalIncome = 0;
+        decimal totalExpense = 0;
+
         public MainWindow()
         {
             InitializeComponent();
+            rbIncome.IsChecked = true;
             BudgetItem grant = new BudgetItem() { Name = "Grant", Amount = 300, Date = new DateTime(2024, 1, 5), Recurring = true };
             lbxIncome.Items.Add(grant.Date.Day.ToString() + " : " + grant.Name + " " + grant.Amount.ToString() + " - (Reccuring)");
+            totalIncome+= grant.Amount;
             BudgetItem bonus = new BudgetItem() { Name = "Bonus", Amount = 300, Date = new DateTime(2024, 1, 15), Recurring = false };
             lbxIncome.Items.Add(bonus.Date.Day.ToString() + " : " + bonus.Name + " " + bonus.Amount.ToString() + " - (One Off)");
+            totalIncome += bonus.Amount;
             BudgetItem wages = new BudgetItem() { Name = "Wages", Amount = 100, Date = new DateTime(2024, 1, 25), Recurring = true };
             lbxIncome.Items.Add(wages.Date.Day.ToString() + " : " + wages.Name + " " + wages.Amount.ToString() + " - (Reccuring)");
+            totalIncome += wages.Amount;
             BudgetItem rent = new BudgetItem() { Name = "Rent", Amount = 400, Date = new DateTime(2024, 1, 1), Recurring = true };
             lbxExpense.Items.Add(rent.Date.Day.ToString() + " : " + rent.Name + " " + rent.Amount.ToString() + " - (Reccuring)");
+            totalExpense += rent.Amount;
             BudgetItem flight = new BudgetItem() { Name = "Flight", Amount = 100, Date = new DateTime(2024, 1, 5), Recurring = false };
-            lbxExpense.Items.Add(flight.Date.Day.ToString() + " : " + flight.Name + " " + flight.Amount.ToString() + " - (Reccuring)");
+            lbxExpense.Items.Add(flight.Date.Day.ToString() + " : " + flight.Name + " " + flight.Amount.ToString() + " - (One Off)");
+            totalExpense += flight.Amount;
             BudgetItem netflix = new BudgetItem() { Name = "Netflix", Amount = 10, Date = new DateTime(2024, 1, 15), Recurring = true };
             lbxExpense.Items.Add(netflix.Date.Day.ToString() + " : " + netflix.Name + " " + netflix.Amount.ToString() + " - (Reccuring)");
+            totalExpense += netflix.Amount;
             BudgetItem spotify = new BudgetItem() { Name = "Spotify", Amount = 8, Date = new DateTime(2024, 1, 20), Recurring = true };
             lbxExpense.Items.Add(spotify.Date.Day.ToString() + " : " + spotify.Name + " " + spotify.Amount.ToString() + " - (Reccuring)");
+            totalExpense += spotify.Amount;
+
+            tblkIncome.Text = totalIncome.ToString();
+            tblkExpense.Text = totalExpense.ToString();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string name;
+            string amountString;
+            decimal amount;
+            DateTime date;
+            bool recurring;
+
+            name = tbxName.Text;
+            amountString = tbxAmount.Text;
+            date = dtpckDate.DisplayDate;
+            if(cbRecurring.IsChecked == true)
+                recurring = true;
+            else
+                recurring = false;
+
+            if(name != "" && decimal.TryParse(amountString, out amount) == true)
+            {
+                if (rbIncome.IsChecked == true)
+                {
+                    if (recurring)
+                        lbxIncome.Items.Add(date.Day + " : " + name + " " + amount + " - (Reccuring)");
+                    else
+                        lbxIncome.Items.Add(date.Day + " : " + name + " " + amount + " - (One Off)");
+
+                    lbxIncome.Items.Refresh();
+                    totalIncome += amount;
+                    tblkIncome.Text = totalIncome.ToString();
+                }
+                else
+                {
+                    if (recurring)
+                        lbxExpense.Items.Add(date.Day + " : " + name + " " + amount + " - (Reccuring)");
+                    else
+                        lbxExpense.Items.Add(date.Day + " : " + name + " " + amount + " - (One Off)");
+
+                    lbxExpense.Items.Refresh();
+                    totalExpense += amount;
+                    tblkExpense.Text = totalExpense.ToString();
+                }
+            }
+        }
+
+        private void rbIncome_Checked(object sender, RoutedEventArgs e)
+        {
+            if(rbIncome.IsChecked == true)
+                rbExpense.IsChecked = false;
+        }
+
+        private void rbExpense_Checked(object sender, RoutedEventArgs e)
+        {
+            if(rbExpense.IsChecked == true)
+                rbIncome.IsChecked = false;
         }
     }
 }
